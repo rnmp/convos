3b914d684aafa539809950bf49a816f52e9d7868
Rolando Murillo <rolando@bandd.co>
2015-09-16 13:04:01 -0400

    smart validation

--- app/models/convo.rb
+++ app/models/convo.rb
@@ -1,13 +1,17 @@
 class Convo < ActiveRecord::Base
 	belongs_to :topic
 	has_many :comments
+	before_validation :smart_add_url_protocol
 	validates :title, presence: true
 	validates :url, presence: true, unless: ->(convo){convo.comment.present?}
-	validates :url, :format => URI::regexp(%w(http https)), unless: ->(convo){convo.comment.present?}
 	validates :comment, absence: true, if: ->(convo){convo.url.present?}
 	acts_as_voteable
 
-	def popularity
-		comments.count
+	protected
+
+	def smart_add_url_protocol
+		unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
+			self.url = "http://#{self.url}"
+		end
 	end
 end
