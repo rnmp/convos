b3286fa892cf75aef4cafc76eaec05f7c4969d73
Rolando Murillo <rolando@bandd.co>
2015-09-27 04:01:59 -0400

    restore smart urls

--- app/models/convo.rb
+++ app/models/convo.rb
@@ -3,7 +3,7 @@ class Convo < ActiveRecord::Base
 	belongs_to :scrape
 	has_many :comments
 
-	after_validation :smart_add_url_protocol
+	before_validation :smart_add_url_protocol
 	validates :title, presence: true
 	validates :url, presence: true, unless: ->(convo){convo.comment.present?}
 	validates :comment, absence: true, if: ->(convo){convo.url.present?}
