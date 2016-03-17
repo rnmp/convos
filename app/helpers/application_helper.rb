4b780a9ad14e5a8f2847c8830deefa6e51b89d26
Rolando Murillo <rolando@bandd.co>
2016-03-17 19:26:24 -0400

    set metadata and markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,20 +4,28 @@ module ApplicationHelper
       filter_html:     true,
       hard_wrap:       true, 
       link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true, 
-      fenced_code_blocks: true
+      with_toc_data: true
     }
 
     extensions = {
       autolink:           true,
       superscript:        true,
-      disable_indented_code_blocks: true
+      space_after_headers: true, 
+      disable_indented_code_blocks: false,
+      fenced_code_blocks: false,
+      tables: true,
+      no_intra_emphasis: true,
+      strikethrough: true
     }
 
     renderer = Redcarpet::Render::HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
 
     markdown.render(text).html_safe
+  end
+
+  def markdown_for_title(text)
+    markdown(text).to_s.gsub!(/<[^>]*>/, "")
   end
 
   def cp(path)
