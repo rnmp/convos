a16fb461ed60e361d77ea79eb82df2683b93581e
Rolando Murillo <rolando@bandd.co>
2016-05-16 22:53:23 -0400

    add better separators

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,6 +3,6 @@ module NotificationsHelper
   	Notification.where({user_id: current_user, read: false })
   end
   def unread_notifications_link
-	  link_to(unread.count.to_s+' reply'.pluralize(unread.count), notifications_path, class: 'unread-link highlight') + "<span class='separator'> • </span>".html_safe if unread.any?
+	  link_to(unread.count.to_s+' reply'.pluralize(unread.count), notifications_path, class: 'unread-link highlight') + "<span class='separator'> | </span>".html_safe if unread.any?
   end
 end
